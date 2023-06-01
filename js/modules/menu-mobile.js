35b06f1b319a2912bd6bc4123aec37f4b996f88c
Pablo107 <pablorossoni107@outlook.com>
2023-05-31 22:33:52 -0300

    feat: upload de arquivos

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,10 @@
+
+    const menuButton = document.querySelector("[data-menu='button']");
+    const menuList = document.querySelector("[data-menu='list']");
+
+    menuButton.addEventListener('click', openMenu);
+
+    function openMenu() {
+        menuList.classList.toggle('active')
+        menuButton.classList.toggle('active')
+    }
---
+++ js/modules/typing.js
@@ -0,0 +1,12 @@
+let letter = 0;
+const text = 'Pablo Rossoni';
+
+function typeWriter() {
+    if(letter < text.length) {
+         document.querySelector(".principal-sentence").innerHTML += text.charAt(letter);
+         letter++;
+        setTimeout(typeWriter, 180)
+        }
+    }
+
+typeWriter();
